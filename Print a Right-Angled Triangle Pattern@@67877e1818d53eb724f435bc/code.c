538a177816b5320a43de1debb604089fc0aca02d
Sarthak Kapoor <sarthakkapoor526@gmail.com>
2025-02-12 22:01:30 +0530

    Solution to Print a Right-Angled Triangle Pattern

--- Print a Right-Angled Triangle Pattern@@67877e1818d53eb724f435bc/code.c
+++ Print a Right-Angled Triangle Pattern@@67877e1818d53eb724f435bc/code.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 void right_triangle(int N){
-    for(int i = 1; i <= 1; i++){
-        for(int j = N; i >= 1; i--){
+    for(int i = 1; i <= N; i++){
+        for(int j = 1; i >= i; i--){
             printf("*");
         }
         printf("\n");
